a516e46d8ddddfc8cdff1e4e3144322702f9eef3
Gavin Yanag <redppl@hotmaill.com>
2013-11-13 19:55:31 +0800

    add

---
+++ C/ex1-4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	double lower = 0;
+	double upper = 300;
+	double step = 20;
+	double celsius = lower;
+	printf("celsius fahr\n\n");
+	while (celsius <= upper){
+		double fahr = celsius * 9.0 / 5.0 + 32.0;
+		printf("%3.0f %6.1f\n", celsius, fahr);
+		celsius += step;
+	}
+	return 0;
+}
+		
