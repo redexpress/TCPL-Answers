8006e14b3dff2eaee7c6da276496dae3db22eb45
Gavin Yanag <redppl@hotmaill.com>
2013-11-13 20:08:59 +0800

    add

--- C/ex1-15.c
+++ C/ex1-15.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+int ftoc(int fahr){
+	return (5.0/9.0) * (fahr-32.0);
+}
+
 int main(){
 	double lower = 0;
 	double upper = 300;
@@ -7,7 +11,7 @@ int main(){
 	double fahr = lower;
 	printf("fahr celsius\n");
 	while (fahr <= upper){
-		double celsius = (5.0/9.0) * (fahr-32.0);
+		double celsius = ftoc(fahr);
 		printf("%3.0f %6.1f\n", fahr, celsius);
 		fahr += step;
 	}
--- C/ex1-4.c
+++ C/ex1-4.c
@@ -5,7 +5,7 @@ int main(){
 	double upper = 300;
 	double step = 20;
 	double celsius = lower;
-	printf("celsius fahr\n\n");
+	printf("celsius fahr\n");
 	while (celsius <= upper){
 		double fahr = celsius * 9.0 / 5.0 + 32.0;
 		printf("%3.0f %6.1f\n", celsius, fahr);
